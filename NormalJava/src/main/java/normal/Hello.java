b3e35195a4da32b537a45c145a0a58eb9540c4ca
heinrich <zhangzimo05004260@gmail.com>
2014-05-09 15:29:27 +0800

    first step

---
+++ NormalJava/src/main/java/normal/Hello.java
@@ -0,0 +1,10 @@
+package normal;
+
+/**
+ * Created by zhangzimo on 14-5-9.
+ */
+public class Hello {
+    public static void main(String args[]){
+        System.out.print("hello World!!!");
+    }
+}
